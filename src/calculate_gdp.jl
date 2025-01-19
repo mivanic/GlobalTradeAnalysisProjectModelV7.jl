0f8e702cf4a5f369862d86d1346275ac2ac068d2
mivanicERS <67010332+mivanicERS@users.noreply.github.com>
2025-01-19 10:40:37 -0500

    Fixed an error

--- src/calculate_gdp.jl
+++ src/calculate_gdp.jl
@@ -1,8 +1,8 @@
 function calculate_gdp(; sets, data0, data1)
 
     return NamedArray((mapslices(sum, data1["qga"] .* data0["pga"] .+ data1["qpa"] .* data0["ppa"] .+ data1["qia"] .* data0["pia"] .+
-                                      mapslices(sum, ifelse.(nan.(data1["qxs"] .* data0["pfob"]), 0.0, data1["qxs"] .* data0["pfob"]), dims=2)[:, 1, :] .+
-                                      mapslices(sum, ifelse.(nan.(data1["qxs"] .* data0["pcif"]), 0.0, data1["qxs"] .* data0["pcif"]), dims=3)[:, :, 1], dims=1)[1, :] .+ data1["pds"]["svces", :] .* data0["qst"][1, :]
+                                      mapslices(sum, ifelse.(isnan.(data1["qxs"] .* data0["pfob"]), 0.0, data1["qxs"] .* data0["pfob"]), dims=2)[:, 1, :] .+
+                                      mapslices(sum, ifelse.(isnan.(data1["qxs"] .* data0["pcif"]), 0.0, data1["qxs"] .* data0["pcif"]), dims=3)[:, :, 1], dims=1)[1, :] .+ data1["pds"]["svces", :] .* data0["qst"][1, :]
         ), sets["reg"])
 
 
