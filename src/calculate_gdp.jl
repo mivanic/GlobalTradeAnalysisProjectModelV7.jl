d686ea08692de67f9aa85aa666c8c9403988ff98
mivanicERS <67010332+mivanicERS@users.noreply.github.com>
2025-01-18 21:56:00 -0500

    Allowed for missing tradein calculate_gdp

--- src/calculate_gdp.jl
+++ src/calculate_gdp.jl
@@ -1,13 +1,9 @@
-function calculate_gdp(; sets, data0, data1, parameters, max_iter=50, constr_viol_tol=1e-5, bound_push=1e-15)
-
+function calculate_gdp(; sets, data0, data1)
 
     return NamedArray((mapslices(sum, data1["qga"] .* data0["pga"] .+ data1["qpa"] .* data0["ppa"] .+ data1["qia"] .* data0["pia"] .+
-                mapslices(sum, data1["qxs"] .* data0["pfob"], dims=2)[:, 1, :] .+
-                mapslices(sum, data1["qxs"] .* data0["pcif"], dims=3)[:, :, 1], dims=1)[1, :] .+ data1["pds"]["svces", :] .* data0["qst"][1, :]
-) ./(mapslices(sum, data0["qga"] .* data0["pga"] .+ data0["qpa"] .* data0["ppa"] .+ data0["qia"] .* data0["pia"] .+
-mapslices(sum, data0["qxs"] .* data0["pfob"], dims=2)[:, 1, :] .+
-mapslices(sum, data0["qxs"] .* data0["pcif"], dims=3)[:, :, 1], dims=1)[1, :] .+ data0["pds"]["svces", :] .* data0["qst"][1, :]
-),sets["reg"])
+                                      mapslices(sum, ifelse.(is.nan.(data1["qxs"] .* data0["pfob"]), 0.0, data1["qxs"] .* data0["pfob"]), dims=2)[:, 1, :] .+
+                                      mapslices(sum, ifelse.(is.nan.(data1["qxs"] .* data0["pcif"]), 0.0, data1["qxs"] .* data0["pcif"]), dims=3)[:, :, 1], dims=1)[1, :] .+ data1["pds"]["svces", :] .* data0["qst"][1, :]
+        ), sets["reg"])
 
-    
+
 end
