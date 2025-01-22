7b1eb8ba1813c77bc48fc81fb7f8925e2ec6db99
mivanicERS <67010332+mivanicERS@users.noreply.github.com>
2025-01-22 16:01:14 -0500

    Corrected error in gdp calculation

--- src/calculate_gdp.jl
+++ src/calculate_gdp.jl
@@ -1,9 +1,10 @@
 function calculate_gdp(; sets, data0, data1)
 
-    return NamedArray((mapslices(sum, data1["qga"] .* data0["pga"] .+ data1["qpa"] .* data0["ppa"] .+ data1["qia"] .* data0["pia"] .+
-                                      mapslices(sum, ifelse.(isnan.(data1["qxs"] .* data0["pfob"]), 0.0, data1["qxs"] .* data0["pfob"]), dims=2)[:, 1, :] .+
-                                      mapslices(sum, ifelse.(isnan.(data1["qxs"] .* data0["pcif"]), 0.0, data1["qxs"] .* data0["pcif"]), dims=3)[:, :, 1], dims=1)[1, :] .+ data1["pds"]["svces", :] .* data0["qst"][1, :]
+    return NamedArray(
+        (
+            mapslices(sum,
+                data1["qga"] .* data0["pga"] .+ data1["qpa"] .* data0["ppa"] .+ data1["qia"] .* data0["pia"] .+
+                mapslices(sum, ifelse.(isnan.(data1["qxs"] .* data0["pfob"]), 0.0, data1["qxs"] .* data0["pfob"]), dims=3)[:, :, 1] .-
+                mapslices(sum, ifelse.(isnan.(data1["qxs"] .* data0["pcif"]), 0.0, data1["qxs"] .* data0["pcif"]), dims=3)[:, 1, :], dims=1)[1, :] .+ mapslices(sum, data1["pds"][sets["marg"], :] .* data0["qst"][sets["marg"], :], dims = 1)[1,:]
         ), sets["reg"])
-
-
 end
