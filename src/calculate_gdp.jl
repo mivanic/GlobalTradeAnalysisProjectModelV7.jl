2def50ba87a977c2b421d75ed91bf1e21b5e0a76
mivanicERS <67010332+mivanicERS@users.noreply.github.com>
2025-01-27 00:07:56 -0500

    Corrected error in GDP calculation

--- src/calculate_gdp.jl
+++ src/calculate_gdp.jl
@@ -5,6 +5,6 @@ function calculate_gdp(; sets, data0, data1)
             mapslices(sum,
                 data1["qga"] .* data0["pga"] .+ data1["qpa"] .* data0["ppa"] .+ data1["qia"] .* data0["pia"] .+
                 mapslices(sum, ifelse.(isnan.(data1["qxs"] .* data0["pfob"]), 0.0, data1["qxs"] .* data0["pfob"]), dims=3)[:, :, 1] .-
-                mapslices(sum, ifelse.(isnan.(data1["qxs"] .* data0["pcif"]), 0.0, data1["qxs"] .* data0["pcif"]), dims=2)[:, 1, :], dims=1)[1, :] .+ mapslices(sum, data1["pds"][sets["marg"], :] .* data0["qst"][sets["marg"], :], dims = 1)[1,:]
+                mapslices(sum, ifelse.(isnan.(data1["qxs"] .* data0["pcif"]), 0.0, data1["qxs"] .* data0["pcif"]), dims=2)[:, 1, :], dims=1)[1, :] .+ mapslices(sum, data0["pds"][sets["marg"], :] .* data1["qst"][sets["marg"], :], dims = 1)[1,:]
         ), sets["reg"])
 end
