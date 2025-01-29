b259111b4d87754bdacc22d909da4294932501bd
mivanicERS <67010332+mivanicERS@users.noreply.github.com>
2025-01-29 07:55:22 -0500

    Error

--- src/calibrate.jl
+++ src/calibrate.jl
@@ -102,7 +102,7 @@ function calibrate(; start_data, data, sets=sets, parameters, fixed, max_iter=10
     fixed["vdpp"] = NamedArray(falses(size(data["vdpp"])), names(data["vdpp"]))
     fixed["vdpp"][1, 1] = true
 
-    mod = model_struc(calibrate_start, sets, parameters, fixed, nothing)
+    mod = model_struct(calibrate_start, sets, parameters, fixed, nothing)
     @time begin
         solve_model!(mod; max_iter=max_iter, constr_viol_tol=constr_viol_tol)
     end
