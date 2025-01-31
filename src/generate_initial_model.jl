2958247583f74575278a9ad499c71dedc589c146
mivanicERS <67010332+mivanicERS@users.noreply.github.com>
2025-01-30 21:45:14 -0500

    Error

--- src/generate_initial_model.jl
+++ src/generate_initial_model.jl
@@ -1,5 +1,5 @@
 function generate_initial_model(; hSets, hData, hParameters)
-    (; sets=sets, parameters, data, fixed) = generate_starting_values(; hSets, hData, hParameters)
+    (; sets, parameters, data, fixed) = generate_starting_values(; hSets, hData, hParameters)
     mc = model_container(JuMP.Model(Ipopt.Optimizer), data, parameters, sets, fixed)
     build_model!(mc)
     return mc
