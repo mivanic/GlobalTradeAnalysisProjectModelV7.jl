144bbddde63dfbcafbf87c1e43528a8a3907f99e
Mitch Phillipson <mitch.phillipson@gmail.com>
2025-02-05 12:50:14 -0600

    Add some math to doc strings

--- src/aggregate_data.jl
+++ src/aggregate_data.jl
@@ -1,5 +1,13 @@
 """
         aggregate_data(; hData, hParameters, hSets, comMap, regMap, endMap)
+
+YOu can type math here ``\int_0^1\,x^2\,dx``. And you can do fancy math
+
+```math
+e^{i\pi} + 1 = 0
+```
+
+This is all in markdown. You can also use latex in the code blocks.
 """
 function aggregate_data(; hData, hParameters, hSets, comMap, regMap, endMap)
 
