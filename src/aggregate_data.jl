face3e6e75726072df0e758f985645fc92a86e2f
mivanicERS <67010332+mivanicERS@users.noreply.github.com>
2025-02-07 11:03:18 -0500

    Improved documentation for aggregate_data

--- src/aggregate_data.jl
+++ src/aggregate_data.jl
@@ -3,22 +3,23 @@
 
 Aggregates data, parameters and sets based on the provided mapping vectors.
 
-        Args:
-                hData: a dictionary of GTAP data (arrays) with the names as found in HAR files
-                hParameters: a dictionary of GTAP parameters (arrays) with the names as found in HAR files
-                hSets: a dictionary of GTAP sets (vectors of strings) with the names as found in HAR files
-                comMap: a mapping vector (NamedVector) which provides for each element in set `comm` the desired aggregate name
-                regMap: a mapping vector (NamedVector) which provides for each element in set `reg` the desired aggregate name
-                endMap: a mapping vector (NamedVector) which provides for each element in set `endw` the desired aggregate name
+### Args:
+- `hData`: a dictionary of GTAP data (arrays) with the names as found in HAR files
+- `hParameters`: a dictionary of GTAP parameters (arrays) with the names as found in HAR files
+- `hSets`: a dictionary of GTAP sets (vectors of strings) with the names as found in HAR files
+- `comMap`: a mapping vector (NamedVector) which provides for each element in set `comm` the desired aggregate name
+- `regMap`: a mapping vector (NamedVector) which provides for each element in set `reg` the desired aggregate name
+- `endMap`: a mapping vector (NamedVector) which provides for each element in set `endw` the desired aggregate name
 
-        Retruns:
-                A named tuple with elements hData, hParameters, hSets containing the aggregated data
+### Retruns:
+
+A named tuple with elements hData, hParameters, hSets containing the aggregated data
         
-        Example:
+### Example:
 
-        ```julia
-        (; hData, hParameters, hSets) = aggregate_data(hData=data, hParameters=parameters, hSets=sets, comMap=comMap, regMap=regMap, endMap=endMap)
-        ```
+`julia
+(; hData, hParameters, hSets) = aggregate_data(hData=data, hParameters=parameters, hSets=sets, comMap=comMap, regMap=regMap, endMap=endMap)
+`
 
 """
 function aggregate_data(; hData, hParameters, hSets, comMap, regMap, endMap)
